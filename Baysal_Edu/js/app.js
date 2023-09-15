71d1fdcadfbe35dc0637ebe7c988abe1f0d00977
Bilal Baysal <bilalbaysal@outlook.com>
2023-09-15 14:59:31 +0300

    contacts done

--- Baysal_Edu/js/app.js
+++ Baysal_Edu/js/app.js
@@ -1,5 +1,11 @@
 
+const slides = document.querySelectorAll(".slide");
+const btnPrev = document.getElementById("prev");
+const btnNext = document.getElementById("next");
 
+let autoSlider = true;
+let intervalTime = 3000;
+let sliderInterval;
 
 $("#my-dropdown").hide();
 $("#btn-profile").click(() => {
@@ -61,3 +67,43 @@ $(window).resize(function () {
     formBtnProfile();
     resizeMyDropdown();
 });
+
+
+btnPrev.addEventListener("click", function () {
+    prevSlide();
+    againInterval();
+});
+btnNext.addEventListener("click", function () {
+    nextSlide();
+    againInterval();
+});
+
+function prevSlide() {
+    let activeSlide = document.querySelector(".active");
+    activeSlide.classList.remove("active");
+    //Eğer activeSlide.previousElementSibling null değilse
+    if (activeSlide.previousElementSibling) {
+        activeSlide.previousElementSibling.classList.add("active");
+    } else {
+        slides[slides.length - 1].classList.add("active");
+    }
+}
+
+function nextSlide() {
+    let activeSlide = document.querySelector(".active");
+    activeSlide.classList.remove("active");
+    if (activeSlide.nextElementSibling && activeSlide.nextElementSibling.classList.contains("slide")) {
+        activeSlide.nextElementSibling.classList.add("active");
+    } else {
+        slides[0].classList.add("active");
+    }
+}
+
+function againInterval() {
+    if (autoSlider) {
+        clearInterval(sliderInterval);
+        sliderInterval = setInterval(nextSlide, intervalTime);
+    }
+}
+
+againInterval();
